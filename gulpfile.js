de1458b950c2b8d45f53e0d96a4758d486447168
antipenko <sweetglory.victory@gmail.com>
2016-07-29 10:46:38 +0300

    main html + gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'), // Подключаем Gulp
+	sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
