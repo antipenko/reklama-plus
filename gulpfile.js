ce5c61fe4aea6b47133c0027641d4fe759f88c0c
antipenko <sweetglory.victory@gmail.com>
2016-08-07 16:09:13 +0300

    finished. need media query

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
-var gulp = require('gulp'), // Подключаем Gulp
-	sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass')
+    browserSync = require('browser-sync'); //Подключаем Sass пакет
+
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('app/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('app/css/'))
+    .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browserSync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: './app/' // Директория для сервера
+        },
+        //notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами
+    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 });
