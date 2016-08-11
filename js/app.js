9f848a5dd022ac3932c131500be823b7d924e91d
antipenko <sweetglory.victory@gmail.com>
2016-08-11 13:13:18 +0300

    add menu scroll

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
 $(document).foundation();
 $(document).ready(function(){
 	$(".rp-scroll").on("click", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
 		event.preventDefault();
 
-		//забираем идентификатор бока с атрибута href
 		var id  = $(this).attr('href'),
 
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
 		top = $(id).offset().top;
 
-		//анимируем переход на расстояние - top за 1000 мс
 		$('body,html').animate({scrollTop: top}, 1000);
 	});
 	$('.menu-icon').click( function(event){
 		$('.rp-scroll').toggle();
 		$('body').toggleClass('rp-body');
+	});
+
+	$("#responsive-menu").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
 	});
 
 });
